990e7ef90ad9e2b84b427a42493fdd1d664911d2
Daisuke-hub <general.daisuke@gmail.com>
2020-03-08 18:01:45 +0900

    [fix] debug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :baria_user, only: [:update]
+	before_action :baria_user, only: [:update, :edit]
+	before_action :authenticate_user!
 
   def show
   	@user = User.find(params[:id])
@@ -8,19 +9,21 @@ class UsersController < ApplicationController
   end
 
   def index
+	@user = current_user
   	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
-  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
+  end
 
   def edit
-  	@user = User.find(params[:id])
+	  @user = User.find(params[:id])
   end
 
   def update
   	@user = User.find(params[:id])
-  	if @user.update(user_params)
-  		redirect_to users_path(@user), notice: "successfully updated user!"
-  	else
-  		render "show"
+	  if @user.update(user_params)
+  		redirect_to user_path(@user), notice: "successfully updated user!"
+	  else
+  		render "edit"
   	end
   end
 
