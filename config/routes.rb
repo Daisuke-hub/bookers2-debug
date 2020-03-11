5f479428da5027cf2f5b8d53e72e3e538e45efac
Daisuke-hub <general.daisuke@gmail.com>
2020-03-12 01:02:13 +0900

    [add] follow機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'relationships/follows'
+  get 'relationships/followers'
+  
   devise_for :users
-  # booksとfavoritesをネスト
+
   resources :books do 
     resource :favorites, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationship, only: [:create, :destroy, :follows, :followers]
+  end
+
   root 'home#top'
   get 'home/about' => "home#about"
-  resources :users,only: [:show,:index,:edit,:update]
 end
